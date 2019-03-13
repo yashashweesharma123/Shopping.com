fd4c8db7c863579bfabe7b5ec270a211022539df
Yashashwee Sharma <45269013+yashashweesharma123@users.noreply.github.com>
2019-03-13 07:39:20 -0700

    Add files via upload

---
+++ SecondFragment.java
@@ -0,0 +1,39 @@
+package test.ojassoft.com.ys;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class SecondFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    View view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_main, container, false);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        addTabs(viewPager);
+        tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        return view;
+
+    }
+    private void addTabs(ViewPager viewPager) {
+
+        PageviewAdapter adapter = new PageviewAdapter(getChildFragmentManager());
+        adapter.addFrag(new MFragment(), "Men");
+        adapter.addFrag(new WFragment(), "Women");
+        adapter.addFrag(new KFragment(), "Kids");
+        viewPager.setAdapter(adapter);
+    }}
+
+
